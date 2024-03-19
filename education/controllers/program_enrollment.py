f0bb13284307150a4e3b5fe2c568e7c859161bf2
basma <bassma.mohamed@brandimic.com>
2024-03-19 19:28:16 +0200

    basma

--- education/controllers/program_enrollment.py
+++ education/controllers/program_enrollment.py
@@ -10,7 +10,17 @@ def get_student_actual_academic_year(doc,event):
     siblings = frappe.get_all("Student Sibling",
 				   filters={"student":doc.student},
 				   pluck='name')
+    frappe.db.set_value("Student",doc.student,"program_enrollment",doc.name)
+    
+    
 				
     for seb in siblings:
         frappe.db.set_value("Student Sibling",seb,"program",doc.program)
-    
+        actual_academic_year = frappe.db.get_value("Student",doc.student,"actual_academic_year")
+        gender = frappe.db.get_value("Student",doc.student,"gender")
+        date_of_birth = frappe.db.get_value("Student",doc.student,"date_of_birth")
+        student_name= frappe.db.get_value("Student",doc.student,"student_name")
+        frappe.db.set_value("Student Sibling",seb,"custom_actual_academic_year",actual_academic_year)
+        frappe.db.set_value("Student Sibling",seb,"full_name",student_name)
+        frappe.db.set_value("Student Sibling",seb,"gender",gender)
+        frappe.db.set_value("Student Sibling",seb,"date_of_birth",date_of_birth)
